30b60160ea7a336c12bba7c14c13732bac87bd79
shenchangqing <shenqinpeng@126.com>
2024-04-17 19:48:14 +0800

    Update

---
+++ README.md
@@ -0,0 +1,20 @@
+## nacos 使用记录
+
+## 测试使用，使用单机模式，standalone
+
+搭建使用 docker-compose, `https://github.com/nacos-group/nacos-docker`
+修改需要认证登录，修改`env/nacos-standalone-mysql.env`
+构建失败，拉不下来 sql 脚本，手动处理，注意构建时的使用相对路径
+
+```
+NACOS_AUTH_ENABLE=true
+# 认证开关，开启后，ui默认nacos/nacos,权限为admin,可以添加新用户后
+# 修改数据库表`roles`,`users`这两个表中的nacos为新用户来禁用nacos用户
+
+## 以下identity_key_value
+# 官方说明，https://nacos.io/zh-cn/blog/announcement-token-secret-key.html
+# 文档这块做的不是很好，这还是我在google上查到的，官方文档的查询是百度，而不是内部查询，LJ
+NACOS_AUTH_IDENTITY_KEY=username
+NACOS_AUTH_IDENTITY_VALUE=shenchangqing
+NACOS_AUTH_TOKEN=SmtjeENZN21nY1ZxdTRYWUN0SktrTEpBdXhuSHZDRUwK # 此值为原32位字符串base64加密后的值，不然启动报错
+```
---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/nacos-group/nacos-sdk-go/v2/clients"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
+)
+
+var (
+	nacos_server_host  = "192.168.1.15"
+	nacos_server_port  = 8848
+	nacos_client_user  = "dev"
+	nacos_client_pwd   = "devdevdev"
+	nacos_client_ns_id = "1b87cfbc-25ee-4981-9494-34db9606b32f" // id of namespace dev
+
+)
+
+type DataConfig struct {
+	Group     string
+	Namespace string
+	DataId    string
+	Data      string
+}
+
+func main() {
+	// create stopCh
+
+	stopCh, cannel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cannel()
+
+	// create server config
+	ns := []constant.ServerConfig{
+		*constant.NewServerConfig(nacos_server_host,
+			uint64(nacos_server_port)),
+	}
+
+	// create client config
+
+	nc := *constant.NewClientConfig(
+		constant.WithUsername(nacos_client_user),
+		constant.WithPassword(nacos_client_pwd),
+		constant.WithNamespaceId(nacos_client_ns_id),
+		constant.WithTimeoutMs(12000),
+		constant.WithNotLoadCacheAtStart(true),
+		constant.WithLogLevel("info"),
+	)
+
+	// create client
+
+	client, err := clients.NewConfigClient(
+		vo.NacosClientParam{
+			ClientConfig:  &nc,
+			ServerConfigs: ns,
+		},
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// get config
+	c1, err := client.GetConfig(vo.ConfigParam{
+		DataId: "test01",
+		Group:  "dev",
+	})
+
+	if err != nil {
+		slog.Error("get config", "msg", err)
+		os.Exit(1)
+	}
+	fmt.Printf("get config test01: %s\n", c1)
+
+	// listen config
+	dataCh := make(chan DataConfig, 1)
+
+	lis_data_id := "test02"
+	lis_group := "dev"
+
+	errListen := client.ListenConfig(vo.ConfigParam{
+		DataId: lis_data_id,
+		Group:  lis_group,
+		OnChange: func(namespace, group, dataId, data string) {
+			dataCh <- DataConfig{
+				Namespace: namespace,
+				Group:     group,
+				DataId:    dataId,
+				Data:      data,
+			}
+		},
+	})
+
+	if errListen != nil {
+		slog.Error("get config", "msg", errListen)
+		os.Exit(1)
+	}
+
+	for {
+		select {
+		case d := <-dataCh:
+			fmt.Printf("config changed, ns: %s, group: %s, dataId: %s, new data: %s\n",
+				d.Namespace, d.Group, d.DataId, d.Data)
+		case <-stopCh.Done():
+			os.Exit(0)
+		}
+	}
+}
